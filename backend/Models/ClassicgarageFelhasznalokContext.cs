09acb152da5ac3467b5d67de235622864f0ea0f3
ponuzsr <ponuzsr@kkszki.hu>
2025-02-01 11:59:04 +0100

    Scaffolded items for users are created

---
+++ backend/Models/ClassicgarageFelhasznalokContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Models;
+
+public partial class ClassicgarageFelhasznalokContext : DbContext
+{
+    public ClassicgarageFelhasznalokContext()
+    {
+    }
+
+    public ClassicgarageFelhasznalokContext(DbContextOptions<ClassicgarageFelhasznalokContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Felhasznalok> Felhasznaloks { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+        => optionsBuilder.UseMySQL("server=localhost;database=classicgarage_felhasznalok;user=root;password=;sslmode=none;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Felhasznalok>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("felhasznalok");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Email).HasMaxLength(40);
+            entity.Property(e => e.Jelszo).HasMaxLength(16);
+            entity.Property(e => e.Nev).HasMaxLength(60);
+            entity.Property(e => e.Prof).HasMaxLength(255);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f0842e141672ce3b641ebdb70b922ade15f7ac4")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

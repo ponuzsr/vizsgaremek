595f4376741062102b3be827ef34d6ab578c7472
ponuzsr <ponuzsr@kkszki.hu>
2025-02-20 07:41:56 +0100

    User template is created

--- backend/Services/Dtos/Dto.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace backend.Services.Dtos
-{
-    public record RegisterRequestDto(string Nev, string Jelszo, string Email, string Prof);
-
-    public record LoginRequestDto(string Nev, string Jelszo);
-}
--- backend/Services/IAuthService/ITokenGenerator.cs
+++
@@ -1,9 +0,0 @@
-﻿using backend.Models;
-
-namespace backend.Services.IAuthService
-{
-    public interface ITokenGenerator
-    {
-        string GenerateToken(Felhasznalok applicationUser, IEnumerable<string> role);
-    }
-}

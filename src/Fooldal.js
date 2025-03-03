d20402c3f2a4d3fe04f9e7c984842824ed3f9af0
pviki <pappv@kkszki.hu>
2025-03-03 12:13:31 +0100

    Home page formatting

--- src/Fooldal.js
+++ src/Fooldal.js
@@ -2,27 +2,37 @@ import React from "react";
 function Fooldal () {
     
   return (
-
+    <div className="body-fooldal">
     <div className="center_texts">
     <div className="home-container">
     <div className="row row-cols-1 row-cols-md-2 g-4">
       <div className="col">
+        
          {/*Bemutatkozás */}
-      <h1>Üdvözöllek a ClassicGarage oldalon!</h1>
-      <p>A ClassicGarage egy olyan oldal ahol az 50-es évektől egészen a 80-as évekig retró autókat találhatunk meg. 
-         Gyártási évét, képét illetve ha a "Részletesebben" gombra rámegyünk még történetét is elolvashatjuk.
-        Bejelentkezés után autóink alá is tudsz kommentelni.
-          Kellemes időtöltést kívánunk!
-      </p>
-      <img/>
+         <div className="introduction">
+      <h1>Üdvözölünk a ClassicGarage oldalon!</h1>
+      <p>A ClassicGarage egy olyan weboldal, amely a múlt autóinak világába repít, és lehetőséget ad arra, 
+        hogy felfedezd az 50-es évektől egészen a 80-as évekig terjedő klasszikus járműveket. 
+        Oldalunkon minden egyes autóhoz megtalálhatod a gyártási évét, fotóit, és ha szeretnéd, a "Részletesebben" gombra 
+        kattintva a történetükről is olvashatsz.
+        A regisztráció után pedig lehetőséged van kommentelni a hozzád legközelebb álló modellek alatt, 
+        így másokkal is megoszthatod véleményed. Fedezd fel velünk a retro autók varázsát!
+        </p>
+        <h1>Kellemes időtöltést kívánunk!</h1>
+        </div>
+      
+      
       </div>
-       {/*Logo */}
+       {/* */}
       <div className="col">
 
       </div>
       </div>
     </div>
   </div>
+  </div>
+  
+    
   );
 };
 
